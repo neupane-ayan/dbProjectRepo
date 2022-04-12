d9a09e31c7db8863996a1f6b6dfb0841fb5280ad
Ayan <ayan@ayans-air.acasmart.jh.edu>
2022-04-12 13:58:04 -0400

    minor changes to double format in sql file

--- setUp.sql
+++ setUp.sql
@@ -4,19 +4,19 @@ CREATE TABLE MetroArea (
         `year` DATETIME,
         gdp DOUBLE,
         averageHousePrice DOUBLE,
-        population BIGINT,
+        population DOUBLE,
         PRIMARY KEY(metroAreaName, `year`)
         );
         
 
 --LOAD DATA
-LOAD DATA LOCAL INFILE '/Users/ayan/Desktop/DB project data/csv_final/table_MetroArea.csv' 
+LOAD DATA LOCAL INFILE '/Users/ayan/Desktop/DB project data/dbProjectRepo/table_MetroArea.csv' 
 INTO TABLE MetroArea
 FIELDS
   TERMINATED BY ','
   ENCLOSED BY '"'
   LINES TERMINATED BY '\n'
-IGNORE 1 ROWS ;
+IGNORE 1 ROWS;
   
   
 --DROP TABLE
