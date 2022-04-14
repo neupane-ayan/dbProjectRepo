f9b612ad7aa85a941b0f4555019cb8fd0a7d783f
Ayan <ayan@ayans-air.acasmart.jh.edu>
2022-04-14 18:26:12 -0400

    new sql file, sorry if i am going to shaft the git

---
+++ setup.sql
@@ -0,0 +1,51 @@
+CREATE TABLE MetroArea (
+        metroAreaName varchar(127),
+        `year` DATETIME,
+        gdp DOUBLE,
+        averageHousePrice DOUBLE,
+        population DOUBLE,
+        PRIMARY KEY(metroAreaName, `year`)
+);
+
+CREATE TABLE PlaysOn (
+        playerID varchar(127),
+        sport varchar(127),
+        teamName varchar(127),
+        `year` datetime,
+        primary key (playerID, sport, teamName, `year`)
+);
+
+CREATE TABLE Player (
+        playerID varchar(127),
+        sport varchar(127),
+        playerName varchar(127),
+        hallOfFame int,
+        primary key (playerID, sport)
+);
+        
+
+LOAD DATA LOCAL INFILE '/Users/ayan/Desktop/DB project data/dbProjectRepo/table_MetroArea.csv' 
+INTO TABLE MetroArea
+FIELDS
+  TERMINATED BY ','
+  ENCLOSED BY '"'
+  LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+LOAD DATA LOCAL INFILE '/Users/ayan/Desktop/DB project data/dbProjectRepo/table_PlaysOn.csv' 
+INTO TABLE PlaysOn
+FIELDS
+  TERMINATED BY ','
+  ENCLOSED BY '"'
+  LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;  
+
+
+LOAD DATA LOCAL INFILE '/Users/ayan/Desktop/DB project data/dbProjectRepo/table_Players.csv' 
+INTO TABLE Player
+FIELDS
+  TERMINATED BY ','
+  ENCLOSED BY '"'
+  LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;  
